1825ec1859c051095213aa74e4f2fbee939317ca
EvgeniyMuhametshin <75341173+EvgeniyMuhametshin@users.noreply.github.com>
2020-12-12 01:58:41 +0300

    Сложно решать без примеровю

---
+++ Lesson3/SApp01/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp01
+{
+	//Мухаметшин Евгений
+
+	//Условия
+	/*а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+	б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;*/
+	class Complex
+	{
+		private double _im;
+		public double Re{get; set;}
+
+		public double Im
+		{
+			get {return _im;}
+			set {
+				if(value > 0)
+					_im = value;
+				else 
+					throw new Exception("Минимальная часть комплексного числа > 0");
+			}
+		}
+
+		public Complex(double re, double im){
+			Re = re;
+			if(im > 0)
+				this._im = im;
+			else
+				throw new Exception("Минимальная часть комплексного числа должна быть > 0");
+		}
+
+		public Complex(){
+		
+		}
+
+		public Complex Plus(Complex x){
+			Complex y = new Complex();
+			y.Re = Re + x.Re;
+			y._im = _im + x._im;
+			return y;
+		}
+
+		public Complex Minus(Complex x){
+			Complex y = new Complex();
+			y.Re = Re - x.Re;
+			y._im = _im - x._im;
+			return y;
+		}
+
+		public Complex Composition(Complex x){
+			Complex y = new Complex();
+			y.Re = Re * x.Re;
+			y._im = _im * x._im;
+			return y;
+		}
+
+		public override string ToString()
+		{
+			return $"{Re} + {_im}i";
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Complex complex1 = new Complex(-2, 5);
+
+			Complex complex2 = new Complex(10, 1);
+
+			Console.WriteLine($"Работа с комплексными числами: {complex1} и {complex2}");
+			Console.WriteLine($"Результат сложения комплексных чисел: {complex1.Plus(complex2)}");
+			Console.WriteLine($"Результат вычетания комплексных чисел: {complex1.Minus(complex2)}");
+			Console.WriteLine($"Результат произведения комплексных чисел: {complex1.Composition(complex2)}");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Lesson3/SApp02/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp02
+{
+    //Мухаметшин Евгений
+
+    //Условия
+   /*а) С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). 
+        Требуется подсчитать сумму всех нечетных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse;
+    б) Добавить обработку исключительных ситуаций на то, что могут быть введены некорректные данные. 
+        При возникновении ошибки вывести сообщение. Напишите соответствующую функцию;*/
+	class Program
+	{   
+
+        #region Проверка ввода с исключением неправельного ввода.
+        static int AddNumbers()
+        {
+            int result = 0;
+            bool _Numbers;
+            do
+            {
+                _Numbers = false;
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                }
+                catch (Exception n)
+                {
+                    _Numbers = true;
+                    string mes = n.Message;
+                    Console.WriteLine("Введите число: " + mes);
+                    Console.Write("Повторите ввод: ");
+                }
+            }
+            while(_Numbers);
+            return result;
+        }
+        #endregion
+
+		static void Main(string[] args)
+		{
+            
+            Console.Write("Введите число: \n" + "Для выходы введите 0\n");
+            int b = 0;
+            while (true)
+            {
+                int a = AddNumbers();
+                if (a == 0)
+                {
+                    break;
+                }
+                else if (a > 0 && a % 2 ==1)
+                {
+                    b++;
+                }
+            }
+            Console.WriteLine("Количество чисел: " + b);
+            Console.ReadLine();
+        }
+	}
+}
