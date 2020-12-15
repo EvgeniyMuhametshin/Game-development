ec071f84dcd1750d05e041298d7ae7c4abdd57cf
EvgeniyMuhametshin <75341173+EvgeniyMuhametshin@users.noreply.github.com>
2020-12-16 01:44:47 +0300

    Второе задание показалось очень сложным

---
+++ Lesson4/SApp01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SApp01
+{
+	
+	//Мухаметшин Евгений
+
+	//Условия
+	/*Дан целочисленный массив из 20 элементов.
+	 Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+	Написать программу, позволяющую найти и вывести количество пар элементов массива, 
+	в которых хотя бы одно число делится на 3. В данной задаче под парой подразумевается два подряд 
+	идущих элемента массива.Например, для массива из пяти элементов: 6; 2; 9; –3; 6 – ответ: 4.*/
+	class Program
+	{
+		static int inputNumbers(int[] array, int length) 
+		{
+
+			int numbers = 0;
+			for (int i = 0; i < length - 1; i++)
+			{
+
+				if (array[i] % 3 == 0 || array[i + 1] % 3 == 0) 
+				{
+
+					numbers++;
+				
+				}
+
+			}
+
+			return numbers;
+		
+		}
+		
+		static void Main(string[] args)
+		{
+
+			const int aLength = 20;
+			int[] mArray = new int[aLength];
+			Random rand = new Random();
+			int result;
+
+			/*Console.WriteLine("");
+			Console.Write("");*/
+			for (int i = 0; i < aLength; i++) 
+			{
+
+				mArray[i] = rand.Next(-10000, 10001);
+				Console.Write(mArray[i] + ",");
+			
+			}
+			Console.WriteLine("");
+			result = inputNumbers(mArray, aLength);
+
+			Console.WriteLine($"Количество пар: {result}");
+			Console.ReadKey();
+
+		}
+		
+	}
+}
---
+++ Lesson4/SApp03/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SApp03
+{
+	//Мухаметшин Евгений
+
+	//Условия
+	/*Решить задачу с логинами из предыдущего урока, 
+	только логины и пароли считать из файла в массив.
+	Создайте структуру Account, содержащую Login и Password.*/
+	class Program
+	{
+		struct Account
+		{
+			public string Login;
+			public string Password;
+
+			public void LoadFromFile(string filename)
+			{
+				filename = "..\\..\\" + filename;
+				StreamReader sr = new StreamReader(filename);
+				Login = sr.ReadLine();
+				Password = sr.ReadLine();
+
+				sr.Close();
+			}
+		}
+
+		/*internal void loadFromFile(string v)
+			{
+				throw new NotImplementedException();
+			}*/
+
+		static bool CheckLogAndPass(Account toCheck)
+		{
+			if (toCheck.Login == "root" && toCheck.Password == "GeekBrains")
+				return true;
+			else
+				return false;
+		}
+
+		static string RightTryWord(int x)
+		{
+			string s = "";
+			// Попытка, когда заканчивается на один, кроме 11.
+			if (x % 10 == 1 && x != 11) s += " попытка";
+			else
+				// Попытки
+				if ((x >= 2 && x <= 4) || (x >= 22 && x <= 24) || (x >= 32 && x <= 34) || (x > 41 && x < 45)) s += " попытки";
+			else
+					// Попыток
+					if ((x == 11) || (x >= 5 && x <= 20) || (x >= 25 && x <= 30) || (x >= 35 && x < 41) || (x > 44 && x < 51)) s += " попыток";
+			return s;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Вас приветствует программа проверки логина и пароля, считанных из файла.");
+			int AmountOfTries = 3;
+
+			string[] fileName = { "data.txt", "tryData.txt", "reallyTryData.txt" };
+
+			Account account;
+			account.Login = "";
+			account.Password = "";
+
+			int i = 0;
+
+			do
+			{
+				Console.WriteLine("\nЗагрузим файл: " + fileName[i]);
+				account.LoadFromFile(fileName[i]);
+
+				Console.Write("Попытка авторизации: ");
+
+				if (CheckLogAndPass(account))
+				{
+
+					break;
+				}
+				else
+				{
+					AmountOfTries--;
+					Console.WriteLine("Неверный ввод логина или пароля." +
+						Environment.NewLine + "У Вас осталось " + AmountOfTries + RightTryWord(AmountOfTries));
+				}
+				i++;
+			} while (AmountOfTries > 0);
+
+			Console.Write("Авторизация успешна!");
+
+			Console.ReadKey();
+		}
+	}
+}
