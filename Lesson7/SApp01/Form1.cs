92c3a843750191f0f2a67d68919ed17e87b3d869
EvgeniyMuhametshin <75341173+EvgeniyMuhametshin@users.noreply.github.com>
2020-12-25 01:37:12 +0300

    Только одна задача

---
+++ Lesson7/SApp01/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SApp01
+{
+    //Мухаметшин Евгений
+
+    //Условия
+   /* а) Добавить в программу «Удвоитель» подсчет количества отданных команд.
+      б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок. Игрок должен постараться получить это число за минимальное количество ходов.
+      в) * Добавить кнопку «Отменить», которая отменяет последние ходы.*/
+
+    public partial class Form1 : Form
+    {
+        Doubler doubler;
+        public Form1()
+        {
+            doubler = new Doubler();
+            InitializeComponent();
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "x2";
+            lblNumber.Text = "0";
+            btnReset.Text = "Сброс";
+            this.Text = "Удвоитель";
+            lblGoal.Visible = false;
+            lblGoalText.Visible = false;
+            lblStepsCount.Text = "0";
+        }
+
+        //Обновление формы
+        public void Update()
+        {
+            lblNumber.Text = doubler.Value.ToString();
+            lblStepsCount.Text = doubler.Steps.ToString();
+            this.Refresh();
+            if (lblGoal.Visible)
+                if (doubler.CheckGoal())
+                {
+                    MessageBox.Show($"Поздравляем, вы достигли заданного числа за {doubler.Steps} ходов");
+                    lblGoal.Visible = false;
+                    lblGoalText.Visible = false;
+                    doubler.Reset();
+                }
+
+        }
+
+        //Увеличение текущего значения на единицу
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            doubler.Increment();
+            Update();
+        }
+
+        //Увеличение текущего значения вдвое
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            doubler.Double();
+            Update();
+        }
+
+        //Сброс текущего значения и счётчика
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            doubler.Reset();
+            Update();
+        }
+
+        //Начинает игру
+        private void menuStart_Click(object sender, EventArgs e)
+        {
+            doubler.GetGoal();
+            MessageBox.Show($"Получите значение: {doubler.Goal}");
+            lblGoal.Visible = true;
+            lblGoalText.Visible = true;
+            lblGoal.Text = doubler.Goal.ToString();
+            doubler.Reset();
+            Update();
+        }
+
+        //Прекращает игру
+        private void menuStop_Click(object sender, EventArgs e)
+        {
+            lblGoal.Visible = false;
+            lblGoalText.Visible = false;
+        }
+
+        //Отменяет последнее действие, кроме сброса
+        private void menuCancel_Click(object sender, EventArgs e)
+        {
+            doubler.CheckStack();
+            Update();
+        }
+
+		private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+		{
+
+		}
+	}
+}
